9bad340628a8149e1f838780ebd12da170d62962
Joshua Menezes <joshua.menezes@outlook.com>
2021-03-21 14:52:29 -0400

    Added base attributes for member class and a new module for the main section of the application

--- Member.py
+++ Member.py
@@ -1,7 +1,31 @@
 from __future__ import annotations
 from typing import List
+from Book import Book
+from datetime import date
+from uuid import uuid4
+
 
 class Member:
     """ A member that belongs to a library
+
+    === Attributes ===
+    name: the name of this member
+    member_id: the 7 digit id of this member
+    dob: the date of birth of this member
+    rented_books: a list of books that this member is currently renting
+
+
     """
-    pass
+    name: str
+    member_id: int
+    dob: date
+    rented_books: List[Book]
+
+    def __init__(self, name, dob: List[str]):
+        self.name = name
+        self.member_id = uuid4().int
+        # Generate a random uuid number in a 128 bit representation
+        self.dob = date(int(dob[0]), int(dob[1]), int(dob[2]))
+        self.rented_books = []
+        
+
---
+++ main.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+from Library import *
+from Member import *
+from Book import *
+
+if __name__ == '__main__':
+    pass
