6876665e4cc484c0b1bdd3166d2c7101522897e2
Joshua Menezes <joshua.menezes@outlook.com>
2021-03-20 22:17:35 -0400

    Added additional functions for creating and removing members

--- Library.py
+++ Library.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 from typing import List, Dict
+from Book import Book
+from Member import Member
 
 
 class Library:
@@ -9,15 +11,16 @@ class Library:
     name: the name of this library
     location: the location of this library
     members: the members of this library
-    books: the current books that this library has
+    catalog: a list of all the books that this library owns
+    available_books: a list of books that are available to rent
 
     """
     name: str
     location: str
 
-    members: List[object]
-    books: List[object]
-    # TODO object placeholders for member and book class
+    members: List[Member]
+    catalog: List[Book]
+    available_books: List[Book]
 
     def __init__(self, name: str, location: str) -> None:
         self.name = name
@@ -25,13 +28,20 @@ class Library:
         self.members = []
         self.books = []
 
-    def add_member(self, member: object) -> None:
+    def add_member(self, member: Member) -> None:
         """ Adds a new member to the library's list of members
 
-        :param member:
-        :return:
+        :param member: The member to add to the library
         """
         if member not in self.members:
             self.members.append(member)
 
-    def remove_member(self, member: object) -> bool:
+    def remove_member(self, member: Member) -> bool:
+        """Removes this member from the library's member list.
+
+        :param member: The member to remove from the library
+        :return: True iff the member was removed. False otherwise.
+        """
+
+        for person in self.members:
+            pass
