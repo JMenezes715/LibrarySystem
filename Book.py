7437e14000efae0363043a8b73d603250636af5f
Joshua Menezes <joshua.menezes@outlook.com>
2021-03-21 22:42:20 -0400

    Added base attributes and initializer method to Book. Created a Queue class for storing members who wish to rent out a book.

--- Book.py
+++ Book.py
@@ -1,7 +1,36 @@
 from __future__ import annotations
 from typing import List
+from datetime import date
+from Queue import Queue
+
 
 class Book:
     """ A book in a library
+
+    === Attributes ===
+    name: the name of this book
+    author: the author(s) of this book
+    publication date: the publication date of this book
+    wait_list: a queue of members waiting to rent the book
     """
-    pass
+
+    name: str
+    author: str
+    publication_date: date
+    wait_list: Queue
+
+    def __init__(self, name: str, author: str, publication_date: List[int]):
+        """ Creates a new Book object
+
+        :param name: the name of the book
+        :param author: the author of the book
+        :param publication_date: a list containing the year, month, and date of
+        publication
+        """
+        self.name = name
+        self.author = author
+        self.publication_date = date(int(publication_date[0]),
+                                     int(publication_date[1]),
+                                     int(publication_date[2]))
+
+        self.wait_list = Queue()
---
+++ Queue.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+from typing import List, Optional, Any
+
+class Queue:
+    """A first-in-first-out (FIFO) queue of items.
+
+    === Attributes ===
+    items: a list of items in the queue
+    """
+    def __init__(self) -> None:
+        """Initialize a new empty queue."""
+        self._items = []
+
+    def is_empty(self) -> bool:
+        """Return whether this queue contains no items.
+
+        >>> q = Queue()
+        >>> q.is_empty()
+        True
+        >>> q.enqueue('hello')
+        >>> q.is_empty()
+        False
+        """
+        return self._items == []
+
+    def enqueue(self, item: Any) -> None:
+        """Add <item> to the back of this queue.
+        """
+        self._items.append(item)
+
+    def dequeue(self) -> Optional[Any]:
+        """Remove and return the item at the front of this queue.
+
+        Return None if this Queue is empty.
+
+        >>> q = Queue()
+        >>> q.enqueue('hello')
+        >>> q.enqueue('goodbye')
+        >>> q.dequeue()
+        'hello'
+        """
+
+        if self.is_empty():
+            return None
+
+        return self._items.pop(0)
